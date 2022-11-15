f64ac1e271f0b450410616482600c7c606eaf9c5
IvanPolarDay <fugasmastafire@gmail.com>
2022-11-15 20:17:10 +0700

    Example01 and 02 is done

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -1,10 +1,31 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 
+
 Console.WriteLine("Введите первое число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-int[] Array = new int[] {number1 , number2};
-Console.WriteLine($"Маскимальное введенное число: {Array.Max()}");
+int[] Array = new int[] { number1, number2 };
+
+Console.WriteLine($"Маскимальное введенное число: {Array.Max()} Мнимальное число: {Array.Min()}");
+
+
+// Альтернативное решение
+/*
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int maxNum = number2;
+int minNum = number1;
+
+if (number1 > number2)
+{
+    maxNum = number1;
+    minNum = number2;
+}
+Console.WriteLine($"Маскимальное введенное число: {maxNum} Мнимальное число: {minNum}");
+*/
--- Example02/Program.cs
+++ Example02/Program.cs
@@ -1,7 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int[] Array = new int[] { number1, number2, number3 };
+
+Console.WriteLine($"Маскимальное введенное число: {Array.Max()}");
 
 
 
+// Альтернативное решение
+/*
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int index = 0;
+
+int[] array = new int[] { number1, number2, number3 };
+
+int maxNum = array[0];
+
+for (index = 0 ; index < array.Length; index++)
+    if (maxNum < array[index])
+    {
+        maxNum = array[index];
+    }
+
+Console.WriteLine($"Маскимальное введенное число: {maxNum}");
+*/
 
