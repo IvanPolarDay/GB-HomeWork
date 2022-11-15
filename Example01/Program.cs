777304e04d19b176e42337d4e56de11b0f7d14a5
Попов Иван <fugasmastafire@gmail.com>
2022-11-15 17:37:02 +0700

    Example01 is done

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int[] Array = new int[] {number1 , number2};
+Console.WriteLine($"Маскимальное введенное число: {Array.Max()}");
