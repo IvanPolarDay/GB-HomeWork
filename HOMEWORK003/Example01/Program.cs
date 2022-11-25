31be9e1343d2ad4b881189d0ab5f8df395b38205
IvanPolarDay <fugasmastafire@gmail.com>
2022-11-25 19:05:23 +0700

    WORK003 Ex01

--- HOMEWORK003/Example01/Program.cs
+++ HOMEWORK003/Example01/Program.cs
@@ -7,3 +7,40 @@
 // 12821 -> да
 
 // 23432 -> да
+
+
+//Это было очень не просто. 
+
+
+Console.WriteLine("Введите пятизначное число");
+
+int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+
+int ConvertNumber(int inputNumber)
+{
+    int convertedNumber = 0;
+
+    while (inputNumber > 0)
+    {
+        convertedNumber = (convertedNumber * 10 + (inputNumber % 10));
+        inputNumber = inputNumber / 10;
+    }
+
+    return convertedNumber;
+}
+
+
+if (inputNumber == ConvertNumber(inputNumber))
+{
+    Console.WriteLine($"{inputNumber} -> да");
+}
+else
+{
+    Console.WriteLine($"{ConvertNumber(inputNumber)} -> нет");
+}
+
+
+
+
+
