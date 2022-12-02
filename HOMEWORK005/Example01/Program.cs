0cd339cf4e4691342fe6eedf922b543364a9ba65
Попов Иван <fugasmastafire@gmail.com>
2022-12-02 22:44:04 +0700

    домашка 5 -все

---
+++ HOMEWORK005/Example01/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int[] array = FillArray(10);
+Console.WriteLine(CountPositiveNumbers(array));
+
+
+int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    foreach (var item in array)
+    {
+        if (item % 2 == 0)
+            count++;
+    }
+    return count;
+}
---
+++ HOMEWORK005/Example03/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] array = FillArray(10);
+
+Console.WriteLine(FindMaxValue(array) - FindMinValue(array));
+
+double[] FillArray(int length)
+{
+    double[] array = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(1, 1000);
+    }
+    return array;
+}
+
+double FindMaxValue(double[] array)
+{
+    double maxValue = 0;
+    for (int i = 0; i < array.Length; i++)
+    if(maxValue < array[i])
+    {
+        maxValue = array[i];
+    }
+        return maxValue;
+}
+
+double FindMinValue(double[] array)
+{
+    double minValue = FindMaxValue(array);
+    for (int i = 0; i < array.Length; i++)
+    if(minValue > array[i])
+    {
+        minValue = array[i];
+    }
+        return minValue;
+}
